967f565fc7a61509fb69572e8165bc4fd08a95de
mar-gitacount <ichikawa.contact@gmail.com>
2024-08-06 16:58:02 +0900

    readmeeditandetc..

--- 2/Expandingtheindex/3/Math.py
+++ 2/Expandingtheindex/3/Math.py
@@ -1,15 +1,11 @@
 from fractions import Fraction
 
-# 分数に変換する。
-# 第一引数に分子、第二引数に分母をいれる
-def FractionMake(molecule,denominator):
-    fraction_result = Fraction(1,molecule**denominator)
-    return fraction_result
+def FractionMake(numerator, denominator):
+    return Fraction(numerator, denominator)
 
+# 計算
+question2 = ((FractionMake(FractionMake(1, 4), 25) * 
+             FractionMake(FractionMake(1, 3), 25)) / 
+             FractionMake(FractionMake(1, 12),25))
 
-
-testresult = FractionMake(9,3)
-result = Fraction((3**2)**-3)
-fraction_result = Fraction(1, 9 ** 3)
-quesion1 = (FractionMake(9,3)*(3 ** 3)/(FractionMake(9,2)))
-print(quesion1)
+print(question2)
