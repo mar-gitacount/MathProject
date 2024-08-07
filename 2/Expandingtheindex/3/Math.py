d5b478044a5c13d115562c0e907085b72543ea58
mar-gitacount <ichikawa.contact@gmail.com>
2024-08-07 16:16:43 +0900

    Math.py編集

--- 2/Expandingtheindex/3/Math.py
+++ 2/Expandingtheindex/3/Math.py
@@ -1,11 +1,17 @@
 from fractions import Fraction
-
+import math
+from sympy import symbols, sqrt
 def FractionMake(numerator, denominator):
+    # 分子=numerator 分母=denominator
     return Fraction(numerator, denominator)
 
-# 計算
-question2 = ((FractionMake(FractionMake(1, 4), 25) * 
-             FractionMake(FractionMake(1, 3), 25)) / 
-             FractionMake(FractionMake(1, 12),25))
+question1 = 3 ** (-6+3+4)
+question2 = 25 ** Fraction(FractionMake(1,4) + FractionMake(1,3) - FractionMake(1,12))
+question3 = 3 ** Fraction(1,2)
 
-print(question2)
+heihouonn = math.sqrt(3)
+print(heihouonn * heihouonn)
+
+# print(question1)
+# print(question2)
+# print(question3)
